9a2a742242123c3bfbbb5a0ef5c94d44efa47a1c
KIT2024AI <aos1435@naver.com>
2024-12-18 18:03:51 +0900

    first_commit

---
+++ study-2.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+
+using namespace std;
+
+class character{
+	public :
+		character(): hp(100), mp(100), Str(5), Dex(5), Int(5), Lux(5){};
+	
+	protected :
+		int hp, mp, Str, Dex, Int, Lux;
+	
+
+		void Show_ablility(){
+
+			for(int i = 0; i +=6; i++)
+			{
+				cout << "~~~";
+			
+			}
+		}
+
+		void attach_damage(){
+
+			cout << "~~~";"
+
+		}
+
+		void Show_Clothes(){
+		
+		// 멤버함수 추가하기
+		Outer.clothes  = ㅇㅇㅇ 
+
+		}
+
+		void ~~~~
+		
+};
+
+class Warrior()
+{
+	~~~
+}
+
+
+			
